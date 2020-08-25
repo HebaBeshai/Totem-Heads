a56a931433b3da51f0ba59c0fc4ba69fa01ab30f
HebaBeshai <hebabeshai@yahoo.com>
2020-08-25 14:51:35 -0400

    Add files via upload

---
+++ TotemHeads.java
@@ -0,0 +1,49 @@
+/**********************************************************************************************************8*********
+ * @file TotemHeads.java
+ * @berief This program uses the method system.out.print to show various totem heads
+ * @author Heba Beshai
+ * @date January 16, 2016
+ ********************************************************************************************************************/
+public class TotemHeads {
+    public static void main(String[] args) {
+        System.out.println("  |||||||||||||||||||");
+        System.out.println("  |                 |");
+        System.out.println("  |                 |");
+        System.out.println("  |    o      o     |");
+        System.out.println(" _|                 |_");
+        System.out.println("|_                   _|");
+        System.out.println("  |  |_ _ _ _ _ _|  |");
+        System.out.println("  |                 |");
+        System.out.println("  |                 |");
+        System.out.println("  |||||||||||||||||||");
+        System.out.println("  |  _ _       _ _  |");
+        System.out.println("  | !   ! _ _ !   ! |");
+        System.out.println("  | !o  !/   \\!o  ! |");
+        System.out.println("  | !_ _!     !_ _! |");
+        System.out.println("  |                 |");
+        System.out.println("  |     ///|\\\\\\     |");
+        System.out.println("   \\               /");
+        System.out.println("    \\      o      /");
+        System.out.println("     \\ _ _ _ _ _ /");
+        System.out.println("      /          \\  ");
+        System.out.println("     /  / \\  / \\  \\");
+        System.out.println("    /   (.)  (.)   \\ ");
+        System.out.println("    |       c      |  ");
+        System.out.println("    |   UUUUUUUU   |        ");
+        System.out.println("    |              |");
+        System.out.println("     \\wwwwwwwwwwww/");
+        System.out.println("      \\wwwwwwwwww/");
+        System.out.println("       \\wwwwwwww/");
+        System.out.println("        \\wwwwww/");
+        System.out.println("         \\wwww/");
+        System.out.println("          \\ww/   ");
+
+
+
+    }
+
+
+
+
+
+}
